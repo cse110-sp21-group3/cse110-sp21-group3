29bef646a7f6153ce01745be31f5becdd1e20d2b
sq19 <shqi@ucsd.edu>
2021-05-15 23:34:08 -0700

    updated edit entry
    bullet type doesn't have the right value in the form for now because type doesn't equal it's html entity

--- source/bullet.js
+++ source/bullet.js
@@ -43,6 +43,89 @@ class BulletLog extends HTMLElement {
                     display: inline-block;
                 }
 
+                /* The Modal (background) */
+                .modal {
+                    display: none; /* Hidden by default */
+                    position: fixed; /* Stay in place */
+                    z-index: 1; /* Sit on top */
+                    left: 0;
+                    top: 0;
+                    width: 100%; /* Full width */
+                    height: 100%; /* Full height */
+                    overflow: auto; /* Enable scroll if needed */
+                    background-color: rgb(0,0,0); /* Fallback color */
+                    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+                }
+                
+                /* Modal Content/Box */
+                .modal-content {
+                    background-color: #efefef;
+                    margin: 15% auto; /* 15% from the top and centered */
+                    padding: 20px;
+                    border: 1px solid #888;
+                    width: 50%; /* Could be more or less, depending on screen size */
+                    border-radius: 20px;
+                }
+
+                #bullet {
+                    border-radius: 20px;
+                    width: 90%;
+                    border: none;
+                    padding: 10px;
+                    margin-bottom: 5px;
+                }
+
+                .selectors {
+                    display: flex;
+                }
+
+                .type {
+                    margin-right: 5%;
+                }
+
+                #bullet-type {
+                    border-radius: 5px;
+                    border: 1px solid #888;
+                    margin-bottom: 5px;
+                    width: 100%;
+                }
+
+                #bullet-modifier {
+                    border-radius: 5px;
+                    border: 1px solid #888;
+                    margin-bottom: 5px;
+                }
+
+                .submit {
+                    display: flex;
+                    justify-content: flex-end;
+                }
+
+                #submitForm {
+                    border-radius: 5px;
+                    border: 1px solid #888;
+                }
+
+                #submitForm:hover {
+                    background-color: #A4A4A4;
+                    color: white;
+                }
+                
+                /* The Close Button */
+                .close-form {
+                    color: #aaa;
+                    float: right;
+                    font-size: 28px;
+                    font-weight: bold;
+                }
+                
+                .close-form:hover,
+                .close-form:focus {
+                    color: black;
+                    text-decoration: none;
+                    cursor: pointer;
+                }
+
         
             </style>
 
@@ -52,6 +135,40 @@ class BulletLog extends HTMLElement {
                 <p class="content">Bullet Content</p>
                 <button class="editBtn">Edit</button>
                 <button class="deleteBtn">Delete</button>
+            </div>
+            
+            <div id="editForm" class="modal">
+            <!-- Modal content -->
+            <div class="modal-content">
+                <span class="close-form">&times;</span>
+                <form>
+                <input type="text" id="bullet" placeholder="Add thoughts here..." maxlength="100">
+                <br>
+                <div class="selectors">
+                    <div class="type">
+                    <label for="bullet-type">Type of bullet</label>
+                    <br>
+                    <select id="bullet-type" name="bullet-type">
+                        <option value="event">event</option>
+                        <option value="task">task</option>
+                        <option value="note">note</option>
+                        <option value="theme">theme</option>
+                    </select>
+                    </div>
+                    <div>
+                    <label for="bullet-type">Modifier</label>
+                    <br>
+                    <select id="bullet-modifier" name="bullet-modifier">
+                        <option value="none">none</option>
+                        <option value="importance">importance</option>
+                        <option value="inspiration">inspiration</option>
+                    </select>
+                    </div>
+                </div>
+                <div class="submit">
+                    <button type="button" id="submitForm">Submit</button>
+                </div>
+                </form>
             </div>`
             ;
 
@@ -67,7 +184,7 @@ class BulletLog extends HTMLElement {
 
     set entry(entry) {
         
-        const modifier = this.shadowRoot.querySelector('.modifier');
+        let modifier = this.shadowRoot.querySelector('.modifier');
         if (entry.modifier === 'importance'){
             modifier.innerHTML = '*';
         } else if (entry.modifier === 'inspiration'){
@@ -76,7 +193,7 @@ class BulletLog extends HTMLElement {
             modifier.innerHTML = " ";
         }
     
-        const type = this.shadowRoot.querySelector('.type');
+        let type = this.shadowRoot.querySelector('.type');
         if (entry.type === 'task') {
             type.innerHTML = '&bull;'
         } else if (entry.type === 'note') {
@@ -87,7 +204,7 @@ class BulletLog extends HTMLElement {
             type.innerHTML = '&#11088;'
         }
         
-        const content = this.shadowRoot.querySelector('.content')
+        let content = this.shadowRoot.querySelector('.content')
         content.innerHTML = entry.content;
 
         this.setAttribute('entry', entry);
--- source/home.js
+++ source/home.js
@@ -24,23 +24,25 @@ function hideMenu(e) {
     document.body.style.overflow = "auto";
 }   
 
-var modal = document.querySelector("#addForm");
+var addForm = document.querySelector("#addForm");
 
 var addBullet = document.querySelector(".add");
-addBullet.addEventListener("click", openForm, false);
+addBullet.addEventListener("click", () => {
+    openForm(addForm);
+});
 
-var span = document.querySelector(".close-form");
-span.addEventListener("click", closeForm, false);
+var addClose = addForm.querySelector(".close-form");
+addClose.addEventListener("click", () => {closeForm(addForm)});
 
-function openForm() {
-    modal.style.display = "block";
+function openForm(form) {
+    form.style.display = "block";
 }
 
-function closeForm() {
-    modal.style.display = "none";
-    document.getElementById('bullet').value = "";
-    document.getElementById('bullet-type').selectedIndex = 0;
-    document.getElementById('bullet-modifier').selectedIndex = 0;
+function closeForm(form) {
+    form.style.display = "none";
+    form.querySelector('#bullet').value = "";
+    form.querySelector('#bullet-type').selectedIndex = 0;
+    form.querySelector('#bullet-modifier').selectedIndex = 0;
 }
 
 
@@ -48,12 +50,12 @@ function closeForm() {
 ** For bullet entries
 */
 
-const submit = document.getElementById('submitForm');
-submit.onclick = () => {
+const submitAdd = addForm.querySelector('.submit #submitForm');
+submitAdd.onclick = () => {
 
-    const modifier = document.getElementById('bullet-modifier').value;
-    const type = document.getElementById('bullet-type').value;
-    const content = document.getElementById('bullet').value;
+    const modifier = addForm.querySelector('#bullet-modifier').value;
+    const type = addForm.querySelector('#bullet-type').value;
+    const content = addForm.querySelector('#bullet').value;
 
     const entry = {
         modifier: modifier,
@@ -78,9 +80,27 @@ function addEntry(entry) {
     bulletLog.entry = entry;
 
     let deleteButton = bulletLog.shadowRoot.querySelector(".deleteBtn");
-    deleteButton.addEventListener('click', () => {deleteEntry(bulletLog)});
+    deleteButton.addEventListener("click", () => {deleteEntry(bulletLog)});
+    let editForm = bulletLog.shadowRoot.querySelector("#editForm");
     let editButton = bulletLog.shadowRoot.querySelector(".editBtn");
-    editButton.addEventListener('click', () => {editEntry(bulletLog)});
+    editButton.addEventListener("click", () => {editEntry(editForm, bulletLog)});
+    let closeEdit = bulletLog.shadowRoot.querySelector(".close-form");
+    closeEdit.addEventListener("click", () => {closeForm(editForm)});
+    let submitEdit = bulletLog.shadowRoot.querySelector("#submitForm");
+    submitEdit.addEventListener("click", () => {
+        const modifier = editForm.querySelector('#bullet-modifier').value;
+        const type = editForm.querySelector('#bullet-type').value;
+        const content = editForm.querySelector('#bullet').value;
+        const entry = {
+            modifier: modifier,
+            type: type,
+            content: content
+        };
+        if (content != "") {
+            bulletLog.entry = entry;
+            closeForm(editForm);
+        }
+    })
 
     dailyLog.prepend(bulletLog);
 
@@ -88,11 +108,38 @@ function addEntry(entry) {
 
 function deleteEntry(bulletLog) {
     const dailyLog = document.getElementById('daily-log-form');
-    console.log(bulletLog);
     dailyLog.removeChild(bulletLog);
 }
 
-function editEntry(bulletLog) {
-    console.log('edit');
+function editEntry(editForm, bulletLog) {
+    let modifier = bulletLog.shadowRoot.querySelector(".modifier").innerHTML;
+    if (modifier == '*') {
+        modifier = 'importance';
+    } else if (modifier == '!') {
+        modifier = 'inspiration';
+    } else {
+        modifier = 'none'
+    }
+    let type = bulletLog.shadowRoot.querySelector(".type").innerHTML;
+    if (type == '&bull;') {
+        type = 'task';
+    } else if (type == '&ndash;') {
+        type = 'note';
+    } else if (type == '&#9702;') {
+        type = 'event';
+    } else {
+        type = 'theme'
+    }
+    let content = bulletLog.shadowRoot.querySelector(".content").innerHTML;
+
+    let bulletModifier = editForm.querySelector('#bullet-modifier');
+    let bulletType = editForm.querySelector('#bullet-type');
+    let bullet = editForm.querySelector('#bullet');
+
+    bullet.value = content;
+    bulletType.value = type;
+    bulletModifier.value = modifier;
+    
+    openForm(editForm);
 }
 
