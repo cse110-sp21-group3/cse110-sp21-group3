ee239165aee89395d169cfbc9b3280920ee7fb63
Aman Aggarwal <akaggarw@ucsd.edu>
2021-05-17 23:56:02 -0700

    Restore tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const enablePurge = process.env.NODE_ENV === 'production';
+module.exports = {
+  purge: {
+    enabled: enablePurge,
+    content: ["source/public/**/*.html", "source/src/**/*.html", "source/src/**/*.js"]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// 
