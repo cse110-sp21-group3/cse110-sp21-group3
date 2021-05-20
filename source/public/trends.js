1fa63a9813f4043c27928431428db79f2a71ae3f
sq19 <shqi@ucsd.edu>
2021-05-20 15:46:19 -0700

    created add functionality for habits

--- source/public/trends.js
+++ source/public/trends.js
@@ -1,10 +1,94 @@
-let add = document.getElementById("add");
-add.addEventListener("click", () => {addHabit()});
+/**
+ * Hamburger menu button
+ */
+ const menuButton = document.querySelector(".menu");
+ menuButton.addEventListener("click", showMenu, false);
+ 
+ /**
+  * Close menu button 
+  */
+ const navSpan = document.querySelector(".close-nav");
+ navSpan.addEventListener("click", hideMenu, false);
+ 
+ /**
+  * Menu 
+  */
+ const flyoutMenu = document.querySelector("#nav");
+ 
+ function showMenu(e) {
+     flyoutMenu.classList.add("show");
+ }
+ 
+ function hideMenu(e) {
+     flyoutMenu.classList.remove("show");
+     e.stopPropagation();
+     document.body.style.overflow = "auto";
+ }
 
-function addHabit() {
-    alert("add habit");
+/**
+ * Add new habit button
+ */
+const addForm = document.querySelector("#addForm");
+
+const addClose = addForm.querySelector(".close-form");
+addClose.addEventListener("click", () => {closeForm(addForm)});
+
+const add = document.getElementById("add");
+add.addEventListener("click", () => {openForm(addForm)});
+
+/*
+** Submit Add Bullet Entry
+*/
+const submitAdd = addForm.querySelector(".submit #submitForm");
+submitAdd.onclick = () => {
+
+    // modifier, type, and content from the form
+    const habit = addForm.querySelector("#habit").value;
+    const color = addForm.querySelector("#color").value;
+
+    // add entry to DOM & localStorage if content isn't empty, using the current UTC date as the key
+    if (habit != "" && color != "") {
+        addHabit(habit, color);
+
+        closeForm(addForm);
+    } else {
+        alert("Please fill in habit and color fields");
+    }
+};
+
+function addHabit(habit, color) {
+    let tracker = document.createElement("tracker-elem");
+    tracker.habit = habit;
+    tracker.color = color;
+    let habitGrid = tracker.shadowRoot.querySelector(".habit-grid");
+    for (let i = 1; i <= 31; i++) {
+        let habitCircle = document.createElement("div");
+        habitCircle.id = "circle"+i;
+        habitCircle.style.borderRadius = "100%";
+        habitCircle.style.border = "none";
+        habitCircle.style.backgroundColor = color;
+        habitGrid.appendChild(habitCircle);
+    }
+    let grid1 = document.getElementById("grid1");
+    grid1.prepend(tracker);
 }
 
+/**
+ * Open and close a modal/form
+ */
+ function openForm(form) {
+    form.style.display = "block";
+}
+
+function closeForm(form) {
+    form.style.display = "none";
+    form.querySelector("#habit").value = "";
+    form.querySelector("#color").value = "";
+}
+
+/**
+ * template testing
+ */
 document.addEventListener("DOMContentLoaded", () => {
     let water = document.createElement("tracker-elem");
     water.habit = "water";
