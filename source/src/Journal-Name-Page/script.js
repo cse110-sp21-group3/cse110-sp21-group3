05eb4b4f0851ae44c99802bcacf46d8e8d7bd978
Eric Chen <erc003@ucsd.edu>
2021-05-19 23:42:56 -0700

    Added functionality to the 'Set Name' page

--- source/src/Journal-Name-Page/script.js
+++ source/src/Journal-Name-Page/script.js
@@ -4,6 +4,7 @@ form.addEventListener("submit", () => {
     let name = document.getElementById("journal-name").value;
     let e = document.getElementById("error");
 
+    // sends an error msg on bad input
     if (name == null || name.trim() === "") {
         e.innerHTML = "Please enter a valid name.";
     } else {
