9fdef4dc326d2ebf12e8c0d00d1e5164e561dd5f
Aman Aggarwal <akaggarw@ucsd.edu>
2021-05-24 05:30:02 -0700

    Modify Name Journal page component

--- source/src/journal-creation/components/JournalIcon.js
+++ source/src/journal-creation/components/JournalIcon.js
@@ -6,15 +6,15 @@ const states = {
 };
 const stateProperties = {
   small: {
-    width: '94px',
-    height: '139px',
+    width: '6rem',
+    height: '9rem',
     labelMarginTop: '1.5rem',
   },
   large: {
-    width: '329px',
-    height: '486px',
+    width: '20.5rem',
+    height: '30rem',
     labelMarginTop: '5rem',
-  }, // TODO: Add another white box behind
+  },
 };
 
 class JournalIcon extends HTMLElement {
@@ -34,15 +34,16 @@ class JournalIcon extends HTMLElement {
   set styleName(styleName) {
     const mainColor = colorThemes[styleName].main;
 
+    let backdropDiv = '<div class="backdrop"></div>';
+    if (this.state === states.small) backdropDiv = '';
     this.shadowRoot.innerHTML = `
             <style>
-                .journal {
+                .journal, .backdrop {
                     width: 67vw;
-                    height: 100vw;  
-
+                    height: 100vw;
+                    
                     margin: auto;
-                    background: ${mainColor};
-                    border: 2px solid ${mainColor};
+
                     box-sizing: border-box;
                     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
                     border-radius: 10px;
@@ -51,7 +52,20 @@ class JournalIcon extends HTMLElement {
                     padding: 0rem;
 
                     cursor: pointer;
-                }            
+                }
+                .journal {
+                    position: relative;
+                    background: ${mainColor};
+                    border: 2px solid ${mainColor};
+                }      
+                .backdrop {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    transform: translate(0.25rem, 0.25rem);
+                    background: white;
+                    z-index: -1;
+                }      
                 .white-box {
                     width: 60%;
                     height: 10%;    
@@ -64,9 +78,9 @@ class JournalIcon extends HTMLElement {
                     border-radius: 5px;
                 }
 
-                                
+                      
                 @media screen and (min-width: 500px) {
-                    .journal {
+                    .journal, .backdrop {
                         width: ${stateProperties[this.state].width};
                         height: ${stateProperties[this.state].height};  
                     }
@@ -75,7 +89,7 @@ class JournalIcon extends HTMLElement {
             </style>
             <div class="journal">
                 <div class="white-box"></div>
-                <div></div>
+                ${backdropDiv}
             </div>
         `;
   }
