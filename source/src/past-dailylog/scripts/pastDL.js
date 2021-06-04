4c73523e98e432182d6a335d5788efd8a18cb654
sq19 <shqi@ucsd.edu>
2021-06-03 21:11:02 -0700

    started creating text editor

--- source/src/past-dailylog/scripts/pastDL.js
+++ source/src/past-dailylog/scripts/pastDL.js
@@ -7,52 +7,62 @@ if (selectedColorStyle === 'null') selectedColorStyle = 'default';
 
 // Set Display CSS Styles
 const root = document.documentElement;
-root.style.setProperty('--main-bg', colorThemes[selectedColorStyle].background);
+root.style.setProperty('--light-bg', colorThemes[selectedColorStyle].background);
+root.style.setProperty('--main-bg', colorThemes[selectedColorStyle].main);
 
-function saveDailyLog() {
-    const data = JSON.parse(localStorage.getItem('dailyLogData')); //dailyLogArchive
-    
-    const bullets = (Object.values(data));
-    bullets.shift();
+function getDailyLog(date) {
+  // TODO: clear old daily log
+  const container = document.querySelector('.past-dailylogs-container');
 
+  // get daily log of date
+  const data = JSON.parse(localStorage.getItem('dailyLogData')); //dailyLogArchive
   
-    bullets.forEach((item) => {
-        let div = document.createElement('div');
-        div.innerHTML = `
-            <style>
-                * {
-                    font-family: 'Nunito', sans-serif;
-                }
-                .past-container {
-                    border: 2px solid black;
-                    margin-top: 1rem;
-                    padding-left: 1rem;
-                    border-radius: 2rem;
-                    box-shadow: 0.5rem 0.5rem;
-                }
-                p {
-                    font-size: 1.5rem;
-                }
-            </style>
-            <div class="past-container">
-                <p>${bulletTypes[item[2]]}&nbsp;${item[0]}</p>
-            </div>
-        `;
-        document.getElementsByClassName('past-dailylogs-container')[0].appendChild(div);
+  const bullets = (Object.values(data));
+  bullets.shift();
 
+  bullets.forEach((item) => {
+      let div = document.createElement('div');
+      div.innerHTML = `
+          <style>
+              p {
+                  font-size: 1.5rem;
+                  margin: 1rem;
+              }
+          </style>
+          <p>${bulletTypes[item[2]]}&nbsp;${item[0]}</p>
+      `;
+      container.appendChild(div);
 
-        const styles = {
-            'font-style': bulletModifiers[item[3]]['font-style'],
-            'font-weight': bulletModifiers[item[3]]['font-weight'],
-            'text-decoration': item[1] ? 'line-through' : 'none',
-        };
-        Object.assign(div.style, styles);
-
-    });
-    
-  
+      const styles = {
+          'font-style': bulletModifiers[item[3]]['font-style'],
+          'font-weight': bulletModifiers[item[3]]['font-weight'],
+          'text-decoration': item[1] ? 'line-through' : 'none',
+      };
+      Object.assign(div.style, styles);
+  });
 }
 
+const title = document.getElementById('title');
+title.addEventListener('change', () => {
+  const [year, month, date] = title.value.split('-');
+  const d = new Date();
+  d.setDate(date);
+  d.setMonth(month - 1);
+  d.setFullYear(year);
+  getDailyLog(d);
+});
+
 document.addEventListener('DOMContentLoaded', () => {
-    saveDailyLog();
+  const d = new Date();
+  let date = d.getDate();
+  if (date < 10) {
+    date = `0${date}`;
+  }
+  let month = d.getMonth() + 1;
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  const today = `${d.getFullYear()}-${month}-${date}`;
+  title.value = today;
+  getDailyLog(d);
 });
