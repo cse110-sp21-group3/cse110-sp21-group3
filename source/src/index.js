bcb7f505f0cf2a6fedb1e95b4cca912f80c1f4e6
Brian Chen <brianchen7193@gmail.com>
2021-06-05 21:19:24 -0700

    Added service workers

--- source/src/index.js
+++ source/src/index.js
@@ -1,5 +1,17 @@
 import { createdJournalKey } from './storageKeys.js';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const createdJournal = localStorage.getItem(createdJournalKey);
   if (createdJournal) {
