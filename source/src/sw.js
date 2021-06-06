edb3c49eeab83cef77146dbc79e354291dbc0586
Brian Chen <brianchen7193@gmail.com>
2021-06-05 21:19:24 -0700

    Added service workers

---
+++ source/src/sw.js
@@ -0,0 +1,44 @@
+var CACHE_NAME = 'bujo-cache';
+var URLS_TO_CACHE = [
+    '/',
+    'https://theme-bujo.netlify.app',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache');
+            return cache.addAll(URLS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then(res => {
+
+            if (res) {
+                return res;
+            }
+
+            return fetch(event.request).then((res) => {
+                if (!res || res.status !== 200 || res.type !== 'basic'){
+                    return res;
+                }
+
+                var responseToCache = res.clone();
+
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseToCache);
+                });
+
+                return res;
+            });
+
+        })
+    );
+});
