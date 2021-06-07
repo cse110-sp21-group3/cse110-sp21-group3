a84f9ab253764c6626d2ed2b305951e37647d63f
Brian Chen <79340264+brichen@users.noreply.github.com>
2021-06-07 08:58:12 -0700

    Update source/src/sw.js
    Co-authored-by: Matei-Alexandru Gardus <matei@gard.us>

--- source/src/sw.js
+++ source/src/sw.js
@@ -34,7 +34,7 @@ self.addEventListener('fetch', (event) => {
 
                 caches.open(CACHE_NAME).then((cache) => {
                     cache.put(event.request, responseToCache);
-                });
+        });
 
                 return res;
             });
