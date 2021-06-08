1f8cb49ec2425f9a94c98d0418c13e846945ac2a
aksharans <aksharan@gmail.com>
2021-06-07 18:32:47 -0700

    fix lint, still some errors

--- source/src/index.js
+++ source/src/index.js
@@ -1,11 +1,11 @@
 import { createdJournalKey } from './storageKeys.js';
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then((registration) => {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, (err) => {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
--- source/src/sw.js
+++ source/src/sw.js
@@ -1,16 +1,16 @@
-var CACHE_NAME = 'bujo-cache';
-var URLS_TO_CACHE = [
-    '/',
+const CACHE_NAME = 'bujo-cache';
+const URLS_TO_CACHE = [
+  '/',
   'https://theme-bujo.netlify.app',
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
       console.log('Opened cache');
       return cache.addAll(URLS_TO_CACHE);
-    })
-    );
+    }),
+  );
 });
 
 self.addEventListener('activate', (event) => {
@@ -19,26 +19,24 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-        caches.match(event.request).then((res) => {
-
+    caches.match(event.request).then((res) => {
       if (res) {
-                return res;
+        return res;
       }
 
       return fetch(event.request).then((res) => {
-        if (!res || res.status !== 200 || res.type !== 'basic'){
-                    return res;
-                }
+        if (!res || res.status !== 200 || res.type !== 'basic') {
+          return res;
+        }
 
-                let responseToCache = res.clone();
+        const responseToCache = res.clone();
 
-                caches.open(CACHE_NAME).then((cache) => {
-                    cache.put(event.request, responseToCache);
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
         });
 
-                return res;
-            });
-
-        })
-    );
+        return res;
+      });
+    }),
+  );
 });
