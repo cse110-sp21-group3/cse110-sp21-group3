bce6988ec9a98ef7999d59b3125c1b12dd8930b8
Brian Seo <bkseo@ucsd.edu>
2021-06-07 17:36:41 -0700

    Add more tests for textEditor

--- __tests__/textEditor.test.js
+++ __tests__/textEditor.test.js
@@ -8,6 +8,20 @@ const bulletData = {
   },
   nestLimit: 2,
 };
+const bulletData2 = {
+  elementName: 'simple-bullet',
+  tree: {
+    0: [1, 2, 3], 1: ['test', []],2: ['test2', []], 3: ['test3', []],
+  },
+  nestLimit: 2,
+};
+const bulletData3 = {
+  elementName: 'simple-bullet',
+  tree: {
+    0: [1, 2, 6], 1: ['a', []],2: ['b', [3]], 3: ['c', [4]], 4: ['d', [5]], 5: ['e', []], 6: ['f', []],
+  },
+  nestLimit: 3,
+};
 describe('Basic tests for TextEditor', () => {
   beforeAll(() => {
     const bList = document.createElement('bullet-list');
@@ -22,9 +36,18 @@ describe('Basic tests for TextEditor', () => {
       elementName: bulletData.elementName,
     });
   });
+  afterAll(() => {
+    document.querySelector('bullet-list').remove();
+  });
+
   test('Test number of top level bullets', () => {
     const topLevelBullets = document.querySelector('bullet-list').querySelectorAll(bulletData.elementName);
     expect(topLevelBullets).toHaveLength(2);
+  });
+  test('Test finding last bullet inside regular bullet', () => {
+    const bList = document.querySelector('bullet-list');
+    const lastBulletID = bList.findLastBulletInside(1);
+    expect(lastBulletID).toBe(1);
   });
   test('Bullet Nesting', () => {
     const topLevelBullets = document.querySelector('bullet-list').querySelectorAll(bulletData.elementName);
@@ -33,7 +56,6 @@ describe('Basic tests for TextEditor', () => {
       0: [1], 1: ['sample', [2]], 2: ['sample2', [3]], 3: ['sample3', []],
     });
   });
-
   test('Bullet Creation', () => {
     const topLevelBullets = document.querySelector('bullet-list').querySelectorAll(bulletData.elementName);
     topLevelBullets[0].createBullet();
@@ -45,7 +67,6 @@ describe('Basic tests for TextEditor', () => {
       4: ['', []],
     });
   });
-
   test('Bullet Editing', () => {
     const bulletElements = document.querySelector('bullet-list').getBulletElements();
     const expectedValue = 'expected Sample';
@@ -71,5 +92,177 @@ describe('Basic tests for TextEditor', () => {
         3: ['sample3', []],
       },
     );
+  });
+  test('Test finding next bullet down of last bullet', () => {
+    const bList = document.querySelector('bullet-list');
+    const nextBulletID = bList.findNextBulletDown(3);
+    expect(nextBulletID).toBe(null);
+  });
+  test('Test finding last bullet inside nest', () => {
+    const bList = document.querySelector('bullet-list');
+    const lastBulletID = bList.findLastBulletInside(2);
+    expect(lastBulletID).toBe(3);
+  });
+});
+
+describe('Tests for Bullet Dataset2', () => {
+  beforeAll(() => {
+    const bList = document.createElement('bullet-list');
+    document.body.appendChild(bList);
+    bList.initialiseList({
+      nestLimit: bulletData2.nestLimit,
+      bulletTree: bulletData2.tree,
+      storageIndex: {
+        value: 0,
+        children: 1,
+      },
+      elementName: bulletData2.elementName,
+    });
+    
+  });
+  afterAll(() => {
+    document.querySelector('bullet-list').remove();
+  });
+  test('Test number of top level bullets', () => {
+    const topLevelBullets = document.querySelector('bullet-list').querySelectorAll(bulletData2.elementName);
+    expect(topLevelBullets).toHaveLength(3);
+  });
+
+  test('Bullet Nesting for dataset 2', () => {
+    const topLevelBullets = document.querySelector('bullet-list').querySelectorAll(bulletData2.elementName);
+    topLevelBullets[1].nestCurrBullet();
+    expect(document.querySelector('bullet-list').getBulletTree()).toMatchObject(
+      {
+        0: [1, 3], 
+        1: ['test', [2]], 
+        2: ['test2', []], 
+        3: ['test3', []],
+    });
+  });
+
+  test('Test Double Nesting of bullet', () => {
+    const topLevelBullets = document.querySelector('bullet-list').querySelectorAll(bulletData2.elementName);
+    topLevelBullets[1].nestCurrBullet();
+    expect(document.querySelector('bullet-list').getBulletTree()).toMatchObject(
+      {
+        0: [1], 
+        1: ['test', [2,3]], 
+        2: ['test2', []], 
+        3: ['test3', []],
+    });
+  });
+  test('Test Nested Bullet Editing', () => {
+    const bulletElements = document.querySelector('bullet-list').getBulletElements();
+    const expectedValue = 'Edited Bullet';
+    bulletElements[2].editContent('value', expectedValue);
+    expect(document.querySelector('bullet-list').getBulletTree()).toMatchObject(
+      {
+        0: [1], 
+        1: ['test', [2,3]], 
+        2: ['Edited Bullet', []], 
+        3: ['test3', []],
+    });
+  });
+  test('Bullet Creation', () => {
+    const topLevelBullets = document.querySelector('bullet-list').querySelectorAll(bulletData.elementName);
+    topLevelBullets[0].createBullet();
+    const bulletElements = document.querySelector('bullet-list').getBulletElements();
+    const expectedValue = 'Bullet 4';
+    bulletElements[4].editContent('value', expectedValue);
+    expect(document.querySelector('bullet-list').getBulletTree()).toMatchObject(
+      {
+        0: [1,4], 
+        1: ['test', [2,3]], 
+        2: ['Edited Bullet', []], 
+        3: ['test3', []],
+        4: ['Bullet 4', []],
+    });
+  });
+  test('Test Nested Bullet Deletion', () => {
+    const bulletElements = document.querySelector('bullet-list').getBulletElements();
+    bulletElements[3].deleteBullet();
+    expect(document.querySelector('bullet-list').getBulletTree()).toMatchObject(
+      {
+        0: [1,4], 
+        1: ['test', [2]], 
+        2: ['Edited Bullet', []], 
+        4: ['Bullet 4', []],
+    });
+  });
+  test('Test finding last bullet inside nest', () => {
+    const bList = document.querySelector('bullet-list');
+    const lastBulletID = bList.findLastBulletInside(1);
+    expect(lastBulletID).toBe(2);
+  });
+  test('Test finding next bullet down', () => {
+    const bList = document.querySelector('bullet-list');
+    const nextBulletID = bList.findNextBulletDown(1);
+    expect(nextBulletID).toBe(2);
+    const nextnextBulletID = bList.findNextBulletDown(2);
+    expect(nextnextBulletID).toBe(4);
+  });
+});
+
+describe('Tests for Bullet Dataset3', () => {
+  beforeAll(() => {
+    const bList = document.createElement('bullet-list');
+    document.body.appendChild(bList);
+    bList.initialiseList({
+      nestLimit: bulletData3.nestLimit,
+      bulletTree: bulletData3.tree,
+      storageIndex: {
+        value: 0,
+        children: 1,
+      },
+      elementName: bulletData3.elementName,
+    });
+    
+  });
+  afterAll(() => {
+    document.querySelector('bullet-list').remove();
+  });
+  test('Test number of top level bullets', () => {
+    const topLevelBullets = document.querySelector('bullet-list').querySelectorAll(bulletData3.elementName);
+    expect(topLevelBullets).toHaveLength(3);
+  });
+  test('Test finding last bullet inside nest', () => {
+    const bList = document.querySelector('bullet-list');
+    const lastBulletID = bList.findLastBulletInside(2);
+    expect(lastBulletID).toBe(5);
+  });
+  test('Test Deletion of Bullet with Nested Elements', () => {
+    const bulletElements = document.querySelector('bullet-list').getBulletElements();
+    bulletElements[2].deleteBullet();
+    console.log(document.querySelector('bullet-list').getBulletTree());
+    expect(document.querySelector('bullet-list').getBulletTree()).toMatchObject(
+      {
+        0: [1,6], 
+        1: ['a', []], 
+        6: ['f', []],
+    });
+  });
+  test('Test finding next bullet down', () => {
+    const bList = document.querySelector('bullet-list');
+    const nextBulletID = bList.findNextBulletDown(1);
+    expect(nextBulletID).toBe(6);
+    const nextnextBulletID = bList.findNextBulletDown(6);
+    expect(nextnextBulletID).toBe(null);
+  });
+  
+  test('Test Bullet Creation into Nest', () => {
+    const topLevelBullets = document.querySelector('bullet-list').querySelectorAll(bulletData3.elementName);
+    topLevelBullets[0].createBullet();
+    console.log(document.querySelector('bullet-list').getBulletTree());
+    const bulletElements = document.querySelector('bullet-list').getBulletElements();
+    const expectedValue = 'New Bullet';
+    bulletElements[7].editContent('value', expectedValue);
+    expect(document.querySelector('bullet-list').getBulletTree()).toMatchObject(
+      {
+        0: [1,7, 6], 
+        1: ['a', []], 
+        7: ['New Bullet', []],
+        6: ['f', []],
+        
+    });
   });
 });
