469a804842d7f8a5f298a14447ba3a9a207ae7cf
Aj <ajslaterhb2000@gmail.com>
2021-06-09 18:49:07 -0700

    closer to main

---
+++ source/index.js
@@ -0,0 +1,26 @@
+import { createdJournalKey } from './storageKeys.js';
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then((registration) => {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, (err) => {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const createdJournal = localStorage.getItem(createdJournalKey);
+  if (createdJournal) {
+    window.location.href = './home-dailylog/home.html';
+  }
+});
+
+const finishOnboardingBtn = document.getElementById('finish-onboarding');
+
+finishOnboardingBtn.addEventListener('click', () => {
+  window.location.href = './journal-creation/journal-picker.html';
+});
